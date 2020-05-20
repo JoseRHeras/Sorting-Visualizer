cb69db069f2574c5541bbd489940db391b7600f6
JoseRHeras <jrheras@yahoo.com>
2020-05-20 15:55:39 -0500

    Bubble sort implementation updated. On slow speeds visualizer works as intended but in high speed synchronization fails. Needs more wokr on it.

--- src/components/BubbleSort.js
+++ src/components/BubbleSort.js
@@ -1,24 +1,24 @@
 export default function BubbleSortColoring(arr){
+
     const coloredArray = [];
     const arrayCopy = arr.slice();
     // var pointA;
     // var pointB;
 
+    /*  */
     for(var i = 0; i < arr.length; i++){
         for(var j = 0; j < arr.length - i - 1; j++){
             // coloredArray.push([j, j + 1]);
-            // var tempArrayContainer = [j, j + 1]
+            var tempArrayContainer = [j, j + 1]
 
             if(arrayCopy[j] > arrayCopy[j + 1]){
 
                 swap(arrayCopy, j, j + 1); 
-                // coloredArray.push([j, j + 1, arrayCopy[j], arrayCopy[j + 1]]); 
-
-                coloredArray.push([j, j + 1, arrayCopy[j], arrayCopy[j + 1]]);
-                // tempArrayContainer.push(arrayCopy[j + 1]);
+                // coloredArray.push([j, j + 1, arrayCopy[j], arrayCopy[j + 1]]);      //Array saves indexes and their repesctive swapped value
+                tempArrayContainer.push(arrayCopy[j], arrayCopy[j + 1]);
             }
 
-            // coloredArray.push(tempArrayContainer);
+            coloredArray.push(tempArrayContainer);
         }
     }
 
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -3,9 +3,9 @@ import GraphColumn from './GraphColumn';
 import BubbleSort from './BubbleSort';
 import './Graph.css'
 
-const PRIMARY_COLOR = 'green';
+const PRIMARY_COLOR = 'red';
 const SECONDARY_COLOR = 'blue';
-const TIME = 0.1;
+const TIME = 800;       //800 testing time
 
 class Graph extends React.Component{
     constructor() {
@@ -32,53 +32,48 @@ class Graph extends React.Component{
 
     //Bubble sort
     bubbleSortVisualizer(){
+        //Perfrom bubble sort and retrieves an array containing sequences to be used in the animation procedure.
         const visualArray = BubbleSort(this.state.columnValues);
-        
-        for(var i = 0; i < visualArray.length; i++){
+        //Creates an array with references to the barColumn which are to be updated.
+        const barStyle = document.getElementsByClassName('column');
 
-            const barStyle = document.getElementsByClassName('column');
+        for(let i = 0; i < visualArray.length; i++){    //Modified const to let
+            //Index to be used to get the correspoding bar elements for barStyle array.
             const[pointAIndex, pointBIndex] = visualArray[i];
 
+            //BarStyle which are updated or not depending of the values from visualArray
             const barOne = barStyle[pointAIndex].style;
             const barTwo = barStyle[pointBIndex].style;
-
-
-            const changeColor = visualArray[i].length > 2;
-            let barOneHeight;
-            let barTwoHeight;
-           
-            console.log(changeColor)
        
-            if(changeColor){
+            //If the array to be executed contains a change this block will be executed
+            if(visualArray[i].length > 2){
 
-                // setTimeout(() => {
-                //     barOne.backgroundColor = SECONDARY_COLOR;
-                //     barTwo.backgroundColor = SECONDARY_COLOR;
-                // }, i * TIME);
+                let barOneHeight = visualArray[i][2];           //variable contains the new height to be assigned to the bar 
+                let barTwoHeight = visualArray[i][3];
 
-
-                barOneHeight = visualArray[i][2];
-                barTwoHeight = visualArray[i][3];
-
-                setTimeout(() => {
-                    barOne.height = barOneHeight + "px";
-                    barTwo.height = barTwoHeight + "px";
-
+                setTimeout(() => {                          //Timer changes and switch colums
+                    barOne.backgroundColor = SECONDARY_COLOR;
+                    barTwo.backgroundColor = SECONDARY_COLOR;
+                    barOne.height = barOneHeight + "px";    //Height of the bar element is change to simutale switch
+                    barTwo.height = barTwoHeight + "px";    //of bars
                 }, i * TIME);
 
-                // setTimeout(() => {
-                //     barOne.backgroundColor = PRIMARY_COLOR;
-                //     barTwo.backgroundColor = PRIMARY_COLOR;
-                // }, i * TIME);
+                setTimeout(() => {
+                    barOne.backgroundColor = PRIMARY_COLOR;
+                    barTwo.backgroundColor = PRIMARY_COLOR;
+                }, (i * TIME) + (TIME * 1.0));//((i * TIME) * 0.05));       //Old time 1000. Adds delay between color switch
 
+            }else{  //If no switch occurs then this bloc executes.
+                setTimeout(() => {
+                    barOne.backgroundColor = SECONDARY_COLOR;
+                    barTwo.backgroundColor = SECONDARY_COLOR;
+                }, i * TIME);
+
+                setTimeout(() => {
+                    barOne.backgroundColor = PRIMARY_COLOR;
+                    barTwo.backgroundColor = PRIMARY_COLOR;
+                }, (i * TIME) + (TIME * 1.0));//((i * TIME) * 0.05));        // 1000 old time. Extra cal creates delay between color switch
             }
-            // else{
-            //     setTimeout(() => {
-            //         barOne.backgroundColor = PRIMARY_COLOR;
-            //         barTwo.backgroundColor = PRIMARY_COLOR;
-            //     }, i * TIME);
-            // }          
-                // console.log(barOneHeight, barTwoHeight);
         }
     }
 
@@ -105,16 +100,11 @@ class Graph extends React.Component{
 function generateArray (){
     const numberArray = [];
 
-    for(var i = 0; i < 800; i++){
+    for(var i = 0; i < 10; i++){
         numberArray.push(Math.round(Math.random() * 1000));
     }
     return numberArray;
 }
 
-export default Graph;
 
-function swap(arr, i, j){
-    var temporalHolder = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temporalHolder;
-}
+export default Graph;
